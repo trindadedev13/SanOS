2ce60e1693dcdb58e0f6bbd4508018e3b614dc81
trindadedev13 <trindadedev13@gmail.com>
2025-07-19 02:14:44 -0300

    chore(asm): Minor improvements

--- src/boot.asm
+++ src/boot.asm
@@ -1,26 +1,30 @@
 [BITS    16]
 [ORG     0x7C00]
 
+; OS Start
 BootMain:
          ; Set 13h Video Mode
-         mov    ah, 0x00    ; set mode
-         mov    al, 0x13    ; 13h
+         mov     ah, 0x00    ; set mode
+         mov     al, 0x13    ; 13h
          int    0x10
+         
+         ; Draw the Welcome
+         mov     si, welcome
+         mov     bl, 0x0A    ; color = green
+         call    DrawString
 
-         mov    si, welcome
-         mov    bl, 0x0A    ; color = green
-         call   DrawString
+         ; Draw the SanOS Text
+         mov     si, sanOS
+         mov     bl, 0x0C    ; color = red
+         call    DrawString
 
-         mov    si, sanOS
-         mov    bl, 0x0C    ; color = red
-         call   DrawString
-
-         jmp    $
+         jmp     $
 
 welcome: db "Welcome to ", 0x00
 sanOS: db "SanOS!", 0x00
 
 %include "src/draw.asm"
 
+; Fill the bytes to fit 512
 times 510 - ($ - $$) db 0x00
 dw    0xAA55
--- src/draw.asm
+++ src/draw.asm
@@ -1,32 +1,31 @@
 [BITS    16]
 
-;
-; Screen
+; ==== Screen ====
 ; 320x200
 ; width = 320
 ; height = 200
-;
+; ================
 
 
 ; al = color
 ; cx = x
 ; dx = y
 SetPixel:
-         mov ah, 0x0C
-         int 0x10
+         mov     ah, 0x0C
+         int     0x10
          ret
 
 ; si = string
 ; bl = color
 DrawString:
-         mov    ah, 0x0E ; Teletype output
-         mov    al, [si] ; al = *si (si[0])
+         mov     ah, 0x0E           ; Teletype output
+         mov     al, [si]           ; al = *si (si[0])
 
          DrawLoop:
-                 int    0x10       ; draw
+                 int    0x10       ; draw current char
 
                  inc    si         ; si++
                  mov    al, [si]   ; al = *si
-                 cmp    al, 0x00   ; compare if al is 0(null)
+                 cmp    al, 0x00   ; compare if al is 0x00 (null)
                  jne    DrawLoop   ; if no, draw
          ret
